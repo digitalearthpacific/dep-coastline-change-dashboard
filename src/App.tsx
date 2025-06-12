fe504cb3d61c4205e45d9617d4734e9736ec7762
tienhoah <nickhoang11@gmail.com>
2025-06-12 16:31:32 -0700

    fix: remove unused import.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 function App() {
   return (
     <div>
-      <div className="container bg-red-500 mx-auto">
+      <div className="container bg-red-500 mx-auto text-center">
         <h1 className="text-8xl text-sky-500">Hello World</h1>
       </div>
     </div>
