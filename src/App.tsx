780ef28db8562e54086b22c32df82e979ddc1d55
tienhoah <nickhoang11@gmail.com>
2025-06-12 16:08:23 -0700

    chore: initial setup with Vite + React + Tailwind CSS v4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <div className="container bg-red-500 mx-auto">
+        <h1 className="text-8xl text-sky-500">Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
