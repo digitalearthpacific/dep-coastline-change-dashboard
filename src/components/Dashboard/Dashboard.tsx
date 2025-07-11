964576c6fc46cbfd22650269b1fa1deffb276817
tienhoah <nickhoang11@gmail.com>
2025-07-11 13:15:57 -0700

    feat: add SearchBar component with country selection and logo

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,16 @@
 import { MainMap } from '../MainMap'
+import { SearchBar } from '../SearchBar'
 import styles from './Dashboard.module.scss'
 
 export const Dashboard = () => {
+  const handleCountrySelect = (countryId: string) => {
+    console.log('Selected country:', countryId)
+    // TODO: Implement country selection logic
+  }
+
   return (
     <div className={styles.dashboardContainer}>
+      <SearchBar onCountrySelect={handleCountrySelect} />
       <MainMap />
     </div>
   )
