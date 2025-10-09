51a6c09c75ae322a562f0287cb58733d35b36238
Justin Robinson <jrobinson@sparkgeo.com>
2025-10-09 11:52:58 -0700

    feat: #150 Add measure tool to map using maplibre-gl-terradraw

--- src/components/MainMap/MainMap.tsx
+++ src/components/MainMap/MainMap.tsx
@@ -7,12 +7,15 @@ import { Flex, IconButton, Text, Tooltip } from '@radix-ui/themes'
 import { Cross1Icon, LayersIcon } from '@radix-ui/react-icons'
 import clsx from 'clsx'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import { MaplibreMeasureControl } from '@watergis/maplibre-gl-terradraw'
+import '@watergis/maplibre-gl-terradraw/dist/maplibre-gl-terradraw.css'
 
 import styles from './MainMap.module.scss'
 import EnterFullScreenIcon from '../../assets/fullscreen.svg'
 import ExitFullScreenIcon from '../../assets/fullscreen-exit.svg'
 import LowQualityShorelineIcon from '../../assets/low-quality-shoreline.svg'
 import WaterRoundedIcon from '../../assets/water-rounded.svg'
+import StraightenRoundedIcon from '../../assets/straighten-rounded.svg'
 import {
   DEFAULT_BBOX,
   FLY_TO_DURATION,
@@ -126,6 +129,7 @@ export const MainMap = ({
   handleHotspotDataChange,
 }: MainMapProps) => {
   const mapRef = useRef<MapRef>(null)
+  const drawRef = useRef<MaplibreMeasureControl>(null)
   const selectedHotspotDataRef = useRef(selectedHotspotData)
   const { isMobileWidth } = useResponsive()
   const { selectedCountryFeature, setContiguousHotspotFeatures } = useMapData()
@@ -134,6 +138,7 @@ export const MainMap = ({
   // State
   const [isDateRangePopupOpen, setIsDateRangePopupOpen] = useState(false)
   const [isBaseMapPopupOpen, setIsBaseMapPopupOpen] = useState(false)
+  const [isMeasuring, setIsMeasuring] = useState(false)
   const [baseMap, setBaseMap] = useState<MapStyleType>('satellite')
   const [isBuildingsLayerVisible, setIsBuildingsLayerVisible] = useState(true)
   const [isMangrovesLayerVisible, setIsMangrovesLayerVisible] = useState(true)
@@ -467,6 +472,20 @@ export const MainMap = ({
     addContiguousHotspot(map)
     addShorelineChangeLayer(map)
 
+    // Add terradraw for measure tools
+    const draw = new MaplibreMeasureControl({
+      modes: ['linestring'],
+      open: false,
+      measureUnitType: 'metric',
+      distancePrecision: 2,
+      forceDistanceUnit: 'auto',
+      areaPrecision: 2,
+      forceAreaUnit: 'auto',
+      computeElevation: true,
+    })
+    map.addControl(draw, 'bottom-right')
+    drawRef.current = draw
+
     // Setup hotspot interactions
     const handleHotspotClick = (e: MapLayerMouseEvent) => {
       if (e.features?.[0]) {
@@ -556,6 +575,30 @@ export const MainMap = ({
     setIsBaseMapPopupOpen(false)
     setIsDateRangePopupOpen((prev) => !prev)
   }, [])
+
+  const handleMeasureTool = useCallback(() => {
+    if (!drawRef.current) return
+
+    if (!isMeasuring) {
+      setIsMeasuring(true)
+      drawRef.current.activate()
+      drawRef.current.getTerraDrawInstance().setMode('linestring')
+    } else {
+      setIsMeasuring(false)
+      const terraDrawInstance = drawRef.current.getTerraDrawInstance()
+
+      // get feature IDs and remove by ID to trigger the removal of
+      // measure labels added by @watergis/maplibre-gl-terradraw
+      const featureIds = terraDrawInstance
+        .getSnapshot()
+        .map((f) => f.id)
+        .filter((id): id is string => typeof id === 'string')
+      terraDrawInstance.removeFeatures(featureIds)
+
+      drawRef.current.resetActiveMode()
+      drawRef.current.deactivate()
+    }
+  }, [isMeasuring])
 
   const handleMapChange = () => {
     const map = mapRef.current?.getMap()
@@ -722,8 +765,14 @@ export const MainMap = ({
       )}
 
       <div className={styles.customMapTools}>
+        <Tooltip content={isMeasuring ? 'Stop Measuring' : 'Measure'} side='left'>
+          <IconButton onClick={handleMeasureTool} aria-label='Measure'>
+            {isMeasuring ? <Cross1Icon /> : <img src={StraightenRoundedIcon} alt='Measure' />}
+          </IconButton>
+        </Tooltip>
+
         <Tooltip content='Adjust Coastlines' side='left'>
-          <IconButton onClick={handleDateRangePopupToggle} aria-label='Adjust Coastlines '>
+          <IconButton onClick={handleDateRangePopupToggle} aria-label='Adjust Coastlines'>
             <img src={WaterRoundedIcon} alt='Coastlines' />
           </IconButton>
         </Tooltip>
